eb06232b604ff7e068a474a82b0c5c30d9d513e0
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-07-11 18:37:38 +0330

    float problem

---
+++ python/12-float problem.py
@@ -0,0 +1,7 @@
+
+#float only in dividing(/) a number that has
+# infinit decimalnumber just count 16 number and roun it sych as 1/3=333...
+#and its priority is not .10 to count correct float numbs
+
+print(1.2-1.0)
+#answer = 0.19999999999999996

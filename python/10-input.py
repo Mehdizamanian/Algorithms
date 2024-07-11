ed87dc2157a3a0a4be037f7b5bf979fdbd18a49d
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-07-11 17:59:40 +0330

    input

---
+++ python/10-input.py
@@ -0,0 +1,13 @@
+
+
+#using input has some tips 
+#1- always converts input in str and has no control on input data
+#2-  to use another data types casting is neccesary
+
+
+
+#write an app that gets an input then print the numbers of input sorry :)
+
+a = int(input("the sorry you need to forgive me :  "))
+
+print("  SORRY  "*a)

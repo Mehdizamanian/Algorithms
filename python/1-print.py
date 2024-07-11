2bc4a50f9ff3d9d35438cfa8c48e5970a72ad272
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-07-11 17:27:41 +0330

    printing

---
+++ python/1-print.py
@@ -0,0 +1,17 @@
+
+
+#printing and kind and messages  str  & int   also some tips (end and sep )
+
+print("we have got ", end="")
+
+print(3 ,end="")
+
+print(" fruit" , end="")
+
+print("apples","orange","banana",sep="-")
+
+
+#print can add    'str'+'str'    or   'str'*pow 
+
+print("we can use pow with string  "*4)
+print("string can plus just by"+"string")

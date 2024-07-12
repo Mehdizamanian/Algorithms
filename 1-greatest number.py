8bfa19f6e30855b5b4ff46ae6b423b8481784504
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-07-12 16:14:00 +0330

    greates one num

---
+++ 1-greatest number.py
@@ -0,0 +1,25 @@
+
+
+#write an app to get 3 numbs to print the greates one 
+
+
+x=int(input('num1 : '))
+y=int(input('num2 : '))
+z=int(input('num3 : '))
+
+
+
+#x y z
+
+if x>=y and x>=z :
+  g=x
+
+elif y>=x and y>=z:
+  g=y
+
+else:
+  g=z
+  
+print(g)
+
+

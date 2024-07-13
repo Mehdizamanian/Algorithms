c765acc30fda8712ef6da57e7fab523ef444886f
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-07-13 16:31:14 +0330

    break and continue in loops

---
+++ Python Turorial Workbranch/22-breakandcontinue.py
@@ -0,0 +1,63 @@
+"""
+diffrence between break and continue  
+both use in all loops (while or for)
+
+
+break = it breaks down and get out of a true block LookupError
+continie = it repeats and jumps from the conditions when being true then break the loop 
+and dosnt run other command bellow 
+
+
+"""
+
+
+
+
+
+#break exercise
+
+'''
+
+
+# x=int(input('Number  :  '))
+
+# i=1
+# while i<=x:
+#   print(i)
+#   i+=1
+
+#   if i==4:
+#     break
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+#continue exercise
+
+'''
+
+x=int(input('Number  :  '))
+
+i=1
+while i<=x:
+  print(i)
+  i+=1
+  continue
+
+  if i==4:
+    break
+    
+
+
+    '''

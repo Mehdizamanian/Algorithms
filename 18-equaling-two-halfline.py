705eeb50f93b678ae8f8caddce20c0489dcfb75c
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-11-20 21:51:01 +0330

    equaling two half line

---
+++ 18-equaling-two-halfline.py
@@ -0,0 +1,12 @@
+
+
+"""الگوریتم و مقایسه طول دو پاره خط و برابر بودن یا نبودن آن ها"""
+
+
+line1=input('line 1 : ')
+line2=input('line 2 : ')
+
+if line1 != line2 :
+  print('not equal') 
+else :
+  print('equal')
